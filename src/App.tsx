a381be8d3110982a619794fa7969a83aa40154c0
Ricardoltao <958990024@qq.com>
2020-08-03 16:40:22 +0800

    Hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import Hello from './components/Hello'
 import LikeButton from './components/LikeButton'
+import MouseTracker from "./components/MouseTracker";
+import useMosePosition from "./hooks/useMosePosition";
 import './App.css';
+import withLoader from './components/withLoader';
+import useURLLoader from './hooks/useURLLoader'
 
-function App() {
+interface IShowResult {
+  message: string;
+  status: string;
+}
+
+// const DogShow: React.FC<{data: IShowResult}> = ({data}) => {
+//   return (
+//     <div>
+//       <h2>Dog Show: {data.status}</h2>
+//       <img src={data.message}/>
+//     </div>
+//   )
+// }
+
+const App = () => {
+  // const WrappedDogShow = withLoader(DogShow, 'https://dog.ceo/api/breeds/image/random')
+  const [show, setShow] = useState(true)
+  // const positions = useMosePosition()
+  // const [data, loading] = useURLLoader('https://dog.ceo/api/breeds/image/random', [show])
+  // const dogResult = data as IShowResult
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <Hello message="Hello React"/>
         <LikeButton/>
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+        {/* <p>
+          <button onClick={() => {setShow(!show)}}>Refresh dog photo</button>
         </p>
+        {
+          loading ? <p>狗 读取中</p> : <img src={dogResult && dogResult.message}/>
+        } */}
         <a
           className="App-link"
           href="https://reactjs.org"
--- src/components/LikeButton.tsx
+++ src/components/LikeButton.tsx
@@ -1,19 +1,41 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, useRef } from 'react'
+// import useMousePositoin from '../hooks/useMosePosition'
 
 const LikeButton: React.FC = () => {
   // const [obj, setObj] = useState({like: 0, on: true})
   const [like, setLike] = useState(0)
   const [on, setOn] = useState(true)
+  const likeRef = useRef(0)
+  const didMountRef = useRef(false)
+  // const positions = useMousePositoin()
   useEffect(() => {
+    console.log('useEffect is running')
     document.title = `点击了${like}次`
+  }, [like])
+  useEffect(() => {
+    if(didMountRef.current) {
+      console.log('this is updated')
+    } else {
+      didMountRef.current = true
+    }
   })
+  
+  function handleAlertclick() {
+    setTimeout(() => {
+      alert('you click on ' + like)
+    }, 3000)
+  }
   return (
     <div>
-      <button onClick={() => {setLike(like + 1)}}>
+      {/* <p>x: {positions.x}, y: {positions.y}</p> */}
+      <button onClick={() => {setLike(like + 1); likeRef.current++}}>
         {like}赞
       </button>
-      <button onClick={() => {setOn(!on)}}>
+      {/* <button onClick={() => {setOn(!on)}}>
         {on ? 'ON' : 'OFF'}
+      </button> */}
+      <button onClick={handleAlertclick}>
+        Alert!
       </button>
     </div>
   )
